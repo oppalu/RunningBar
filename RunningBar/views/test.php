b0b173001e3a52ad38fdbdb7afdde951e5440fd3
oppalu <141250037@smail.nju.edu.cn>
2016-11-02 11:23:23 +0800

    好久不写了先建个表

---
+++ RunningBar/views/test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: phoebegl
+ * Date: 2016/10/29
+ * Time: 23:36
+ */
+
+include("header.html");
+
+include("test.html");
+
+include("footer.html");
+
+
+?>
