41894d0e8d13cfe06cd7814474a11c1f1e96bf80
oppalu <141250037@smail.nju.edu.cn>
2016-11-07 10:45:05 +0800

    跟apache死磕到底[手动再见]

--- RunningBar/index.php
+++ RunningBar/index.php
@@ -1,47 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>首页</title>
-
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <link rel="stylesheet" href="View/common/assets/css/bootstrap.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-    <link rel="stylesheet" href="View/common/assets/css/App.css">
-    <link rel="stylesheet" href="View/common/assets/css/skins/allskins.css">
-
-    <script src="View/common/assets/js/jquery.min.js"></script>
-    <script src="View/common/assets/js/bootstrap.min.js"></script>
-    <script src="View/common/assets/js/app.js"></script>
-</head>
-
-<body style="background-image: url(assets/img/index.png);background-size: cover">
-<div class="row">
-    <div class="col-md-1 col-sm-1">
-        <img src="View/common/assets/img/logo.png" style="width: 80%">
-    </div>
-
-    <div class="col-md-4 col-sm-4 pull-right" style="margin: 7% auto;">
-        <p class="h01">
-            <strong>Running</strong>&nbsp;Bar
-        </p>
-        <h1 class="pull-right">——奔跑遇见更好的你</h1>
-        <p>
-        <h4>
-            Running Bar为你记录分析运动数据,并让你找到志同道合的伙伴,与好友一同分享运动的乐趣。
-        </h4>
-        </p>
-        <div class="center-block col-md-6 col-sm-6" style="margin: 7% auto;">
-            <a href="View/login.php" class="center-block">
-                <button class="btn btn-app btn-block btn-flat" style="font-size: large">
-                    开启你的运动之旅
-                </button>
-            </a>
-        </div>
-
-    </div>
-</div>
-
-</body>
-</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: phoebegl
+ * Date: 2016/11/7
+ * Time: 09:43
+ */
